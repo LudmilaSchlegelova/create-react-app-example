ff54c9b065275982f38f2ca1190ed2868488cae3
Ľudmila Jalčová <lu.jalcova@gmail.com>
2020-05-19 16:42:54 +0200

    Updates

---
+++ precache-manifest.796b6dc0a0e0c274de91df9e07923745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3a1c35551013866fa3150ec98c0ffa",
+    "url": "/create-react-app-example/index.html"
+  },
+  {
+    "revision": "aaa8df44740f9417a220",
+    "url": "/create-react-app-example/static/css/main.41baa62d.chunk.css"
+  },
+  {
+    "revision": "f9084a1b06e4e282a39a",
+    "url": "/create-react-app-example/static/js/2.438a1e66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app-example/static/js/2.438a1e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa8df44740f9417a220",
+    "url": "/create-react-app-example/static/js/main.fc35aee6.chunk.js"
+  },
+  {
+    "revision": "4e376f2b58f588b3d2e1",
+    "url": "/create-react-app-example/static/js/runtime-main.7db39acd.js"
+  }
+]);
